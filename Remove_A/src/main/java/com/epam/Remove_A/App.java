9014603935093597c7fb143d2a8fafe57d7696e0
Yamini Seepana <177r1a05a6@gmail.com>
2020-03-25 11:06:01 +0530

    Junit_task

---
+++ Remove_A/src/main/java/com/epam/Remove_A/App.java
@@ -0,0 +1,13 @@
+package com.epam.Remove_A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
